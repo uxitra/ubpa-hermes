3b498318e123a1ec759a9e314e5ee1a26ae07d5f
Benjamin Renz <benjamin.l.renz@gmail.com>
2025-11-03 14:20:29 +0100

    Add build step for tailwind css

---
+++ build.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+
+fn main() {
+    println!("cargo:rerun-if-changed=static/html/input.css");
+    println!("cargo:rerun-if-changed=tailwind.config.js");
+
+
+    // Run tailwind build command
+    let status = Command::new("npx")
+        .args([
+            "tailwindcss",
+            "-i", "./static/html/input.css",
+            "-o", "./static/html/output.css",
+            "--minify",
+        ])
+        .status()
+        .expect("Failed to run Tailwind build command");
+
+    if !status.success() {
+        panic!("Tailwind build failed!");
+    }
+}
